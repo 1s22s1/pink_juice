efcb35a5acae2ef8ec6a480ac65fc67c53fa8f73
1s22s1 <1s22s1@users.noreply.github.com>
2025-03-25 18:10:41 +0900

    :up:

---
+++ yatteiki/spec/factories/tasks.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :task do
+    title { "作業その１" }
+    description
+  end
+end
---
+++ yatteiki/spec/models/task_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe '#valid?' do
+    let(:task) { Task.build(title: title, description: description) }
+
+    subject { task.valid? }
+
+    describe '有効な入力' do
+      let(:title) { '作業名を記載する' }
+      let(:description) { '説明を記載する' }
+      
+      it { is_expected.to eq true }
+    end
+
+    describe '無効な入力' do
+      context 'タイトルを入力しない場合' do
+        let(:title) { nil }
+        let(:description) { nil }
+
+        it { is_expected.to eq false }
+      end
+    end
+  end
+end
