0ca9fc1a2bc568c514b8ffb40b719f17a098b0ba
1s22s1 <1s22s1@users.noreply.github.com>
2025-03-27 09:06:42 +0900

    :up:

---
+++ abc116_c.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n';
+
+int init()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    return 0;
+}
+
+int main()
+{
+    init();
+
+    int N;
+    cin >> N;
+
+    vector<int> h(N);
+    for (auto i : ranges::views::iota(0, N))
+    {
+        cin >> h.at(i);
+    }
+
+    int answer = 0;
+
+    for (int height = 1; height <= *max_element(h.begin(), h.end()); height++)
+    {
+        vector<int> line(N);
+        for (auto i : ranges::views::iota(0, N))
+        {
+            if (h.at(i) < height)
+            {
+                line.at(i) = 0;
+            }
+            else
+            {
+                line.at(i) = 1;
+            }
+        }
+
+        line.erase(unique(line.begin(), line.end()), line.end());
+        answer += count(line.begin(), line.end(), 1);
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
