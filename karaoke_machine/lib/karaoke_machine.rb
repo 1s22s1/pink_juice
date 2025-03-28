abdb1bd9d1f5b85ea120707a16cb7200aeb0eb5c
1s22s1 <1s22s1@users.noreply.github.com>
2025-03-28 12:23:33 +0900

    :up:

--- karaoke_machine/lib/karaoke_machine.rb
+++ karaoke_machine/lib/karaoke_machine.rb
@@ -1,13 +1,17 @@
 class KaraokeMachine
+  KEYBOARD_PATTERN = %w(C C# D D# E F F# G G# A A# B).freeze
+
   def initialize(melody)
     @melody = melody
   end
 
   def transpose(key)
-    @melody.scan(/\|/) do |pattern|
-      if ['|'].include?(pattern)
+    @melody.scan(/\|/).map do |pattern|
+      if ['|', ' '].include?(pattern)
         pattern
+      else
+        KEYBOARD_PATTERN[KEYBOARD_PATTERN.find_index(pattern) + 1]
       end
-    end
+    end.join
   end
 end
--- karaoke_machine/spec/karaoke_machine_spec.rb
+++ karaoke_machine/spec/karaoke_machine_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe KaraokeMachine do
 
       it { is_expected.to eq '|' }
     end
+
+    xcontext '半角スペースを入力した場合' do
+      let(:melody) { ' ' }
+      let(:key) { 1 }
+
+      it { is_expected.to eq ' ' }
+    end
+
+    xcontext '1キーを上げる場合' do
+      let(:melody) { 'C#' }
+      let(:key) { 1 }
+
+      it { is_expected.to eq 'D' }
+    end
   end
 end
